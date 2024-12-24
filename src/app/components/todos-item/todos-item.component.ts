fa0e4ffb959421f1671641493ac5073f5d4e96e8
khalid.zennouhi <144844220+KHALID-ZENNOUHI@users.noreply.github.com>
2024-12-24 19:11:26 +0100

    Add TodosItemComponent with template, styles, and unit tests

---
+++ src/app/components/todos-item/todos-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todos-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './todos-item.component.html',
+  styleUrl: './todos-item.component.scss'
+})
+export class TodosItemComponent {
+
+}
